d2b056ab3a9c5d16db406ea3c96f77e7662fcafa
Trevor.Max <Trevor.Max@btg.co.nz>
2020-10-06 15:50:23 +1300

    Issue #main - frontend framework is finished

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id="App">
+        <div id="sidebar">
+        </div>
+        <div id="header">
+        </div>
+        <div id="body">
+        </div>
+        <div id="footer">
+        </div>
+    </div>
+  );
+}
+
+export default App;
